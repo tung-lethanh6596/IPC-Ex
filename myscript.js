f2da783a156357facb8189f069dbadb82c8efc1e
tung-lethanh6596 <tung.lethanh6596@gmail.com>
2017-09-17 17:15:26 +0700

    All

--- myscript.js
+++ myscript.js
@@ -1,8 +1,19 @@
 const TASKS = document.getElementById('tasks');
 
-var myOption = document.createElement("div");
-myOption.setAttribute("class", "task");
-var optionText = document.createTextNode("My Option");
-myOption.appendChild(optionText);
+var marioTask = document.createElement("div");
+marioTask.setAttribute("class", "task");
 
-TASKS.appendChild(myOption);
+var marioLink = document.createElement("a");
+marioLink.setAttribute("class", "task-icon-link");
+marioLink.setAttribute("href", "_blank");
+
+var marioIMG = document.createElement("img");
+marioIMG.setAttribute("class", "icon-mario");
+marioIMG.setAttribute("src", "mario.ico");
+marioIMG.setAttribute("style", "width: 24px; height: 24px; margin: 2px;");
+
+marioLink.appendChild(marioIMG);
+
+marioTask.appendChild(marioTask);
+
+TASKS.appendChild(marioTask);
