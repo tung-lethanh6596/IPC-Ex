4729c5bf966d56bb2e5e6a31dea1773d20aa296d
tung-lethanh6596 <tung.lethanh6596@gmail.com>
2017-10-15 16:37:29 +0700

    new

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo $1 |sed 's/;/\n/g' > tmp.txt
+testcases=""
+testfiles=""
+oldtestfile=""
+while read -r test
+do
+    cnt=`echo $test | sed 's/\//\n/g' | wc -l`
+    cnt=$(($cnt - 1))
+    before=$(($cnt - 1))
+    testcase=`echo $test | cut -d'/' -f$cnt`
+    testfile=`echo $test | cut -d'/' -f1-$before`
+    testcases="$testcases -t \"$testcase\""
+    if [ "$testfile" != "$oldtestfile" ]
+    then
+    	testfiles="$testfiles $testfile"
+    fi
+    oldtestfile=$testfile
+done < tmp.txt
+script="robot $testcases $testfiles"
+echo $script
+rm -f tmp.txt
