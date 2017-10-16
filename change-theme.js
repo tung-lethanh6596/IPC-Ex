d7e1d65aca373b7aa62fc7caf572b04d835ee4f6
tung-lethanh6596 <tung.lethanh6596@gmail.com>
2017-10-16 17:39:02 +0700

    new

---
+++ change-theme.js
@@ -0,0 +1,3 @@
+document.querySelector("#breadcrumbs").innerHTML = document.querySelector("#breadcrumbs").innerHTML.replace("Jenkins", "vttekCI");
+document.querySelector(".jenkins_ver a").innerHTML = "vttekCI v0.3";
+document.querySelectorAll("[href='/manage']")[2].innerHTML = "Manage vttekCI";
